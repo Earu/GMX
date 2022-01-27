22c2b5c68818a69ad78c3f8a5485f28ed291a9b7
Earu <earu.derg@gmail.com>
2022-01-27 19:58:09 +0100

    fix a few issues with the firewall

--- lua/gmx/gmx_scripts/menu/http_firewall.lua
+++ lua/gmx/gmx_scripts/menu/http_firewall.lua
@@ -2,25 +2,28 @@ require("http_filter")
 
 local firewall_rules = {
 	-- weird
-	["garrysmod.io"] = { method = "*", type = "DENY" },
+	["garrysmod.io"]     = { method = "*", type = "DENY" },
 	["dl.cloudsmith.io"] = { method = "*", type = "DENY" },
 
 	-- dev stuff
 	["dl.dropboxusercontent.com"] = { method = "GET", type = "ALLOW" },
-	["api.github.com"] = { method = "*", type = "ALLOW" },
-	["raw.githubusercontent.com"] = { method = "*", type = "ALLOW" },
-	["gitlab.com"] = { method = "*", type = "ALLOW" },
-	["api.github.com"] = { method = "*", type = "ALLOW" },
+	["api.github.com"]            = { method = "*",   type = "ALLOW" },
+	["raw.githubusercontent.com"] = { method = "*",   type = "ALLOW" },
+	["gitlab.com"]                = { method = "*",   type = "ALLOW" },
+	["api.github.com"]            = { method = "*",   type = "ALLOW" },
 
 	-- trusted
-	["api.betterttv.net"] =  {method = "GET", type = "ALLOW" },
+	["api.betterttv.net"]    = { method = "GET", type = "ALLOW" },
 	["translate.yandex.net"] = { method = "GET", type = "ALLOW" },
 	["api.frankerfacez.com"] = { method = "GET", type = "ALLOW" },
+	["drive.google.com"]     = { method = "GET", type = "ALLOW" },
+	["i.imgur.com"]          = { method = "GET", type = "ALLOW" },
 
 	-- metastruct
-	["g1.metastruct.net"] = { method = "*", type = "ALLOW" },
-	["g2.metastruct.net"] = { method = "*", type = "ALLOW" },
-	["g3.metastruct.net"] = { method = "*", type = "ALLOW" },
+	["g1.metastruct.net"]    = { method = "*",   type = "ALLOW" },
+	["g2.metastruct.net"]    = { method = "*",   type = "ALLOW" },
+	["g2cf.metastruct.net"]  = { method = "*",   type = "ALLOW" },
+	["g3.metastruct.net"]    = { method = "*",   type = "ALLOW" },
 	["metastruct.github.io"] = { method = "GET", type = "ALLOW" },
 }
 
@@ -33,11 +36,11 @@ hook.Add("OnHTTPRequest", "gmx_http_firewall", function(url, method)
 			return true
 		end
 
-		if rule.type == "ALLOW" and rule.method ~= method then
+		if rule.type == "ALLOW" and rule.method ~= method and rule.method ~= "*" then
 			gmx.Print("HTTP request blocked: ", method, url)
 			return true
 		end
+	else
+		gmx.Print("Blocking request because no rule was defined", method, domain, url)
 	end
-
-	gmx.Print("No rule defined for ", method, domain, url)
 end)
