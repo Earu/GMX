2e3291c2bff6ff3d8ed4a08abe7a37153fa0528e
Earu <earu.derg@gmail.com>
2022-02-18 22:17:36 +0100

    gmx custom menu

---
+++ lua/menu/menu.lua
@@ -0,0 +1,13 @@
+if jit.arch ~= "x64" then
+	include("_menu.lua")
+end
+
+include("menu_save.lua")
+include("menu_demo.lua")
+include("menu_addon.lua")
+include("menu_dupe.lua") -- called by the engine, cant remove
+include("errors.lua") -- "something is creating script errors pop up"
+
+include("gmx/gmx.lua")
+
+require("xconsole")
