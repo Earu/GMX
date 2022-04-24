13bd487f89466a2f5214a04e8011b3ba43911cc5
Earu <earu.derg@gmail.com>
2022-04-24 14:34:19 +0200

    fix whitelist

--- lua/gmx/gmx_scripts/menu/srv_whitelist.lua
+++ lua/gmx/gmx_scripts/menu/srv_whitelist.lua
@@ -1,27 +1,24 @@
+require("sourcenet")
+
 local host_ip_cvar = GetConVar("hostip")
-local cached_ip
-local next_cache_update = 0
 function gmx.GetIPAddress(force)
-	if force or not cached_ip or CurTime() >= next_cache_update then
-		local host_ip = host_ip_cvar:GetInt()
-		local ip = {}
-
-		ip[1] = bit.rshift(bit.band(host_ip, 0xFF000000), 24)
-		ip[2] = bit.rshift(bit.band(host_ip, 0x00FF0000), 16)
-		ip[3] = bit.rshift(bit.band(host_ip, 0x0000FF00), 8)
-		ip[4] = bit.band(host_ip, 0x000000FF)
-
-		cached_ip = table.concat(ip, ".")
-		next_cache_update = CurTime() + 0.5
+	if IsInGame() then
+		local chan_addr = CNetChan():GetAddress()
+		if chan_addr then
+			return tostring(chan_addr)
+		end
 	end
 
-	return cached_ip
-end
+	local host_ip = host_ip_cvar:GetInt()
+	local ip = {}
 
-local cur_ip_addr = gmx.GetIPAddress()
-hook.Add("ClientStateCreated", "gmx_srv_whitelist", function()
-	cur_ip_addr = gmx.GetIPAddress(true)
-end)
+	ip[1] = bit.rshift(bit.band(host_ip, 0xFF000000), 24)
+	ip[2] = bit.rshift(bit.band(host_ip, 0x00FF0000), 16)
+	ip[3] = bit.rshift(bit.band(host_ip, 0x0000FF00), 8)
+	ip[4] = bit.band(host_ip, 0x000000FF)
+
+	return table.concat(ip, ".")
+end
 
 local WHITELIST = {
 	["0"] = true, -- menu
@@ -30,5 +27,5 @@ local WHITELIST = {
 
 function gmx.IsGameWhitelisted()
 	if not IsInGame() then return true end
-	return WHITELIST[cur_ip_addr] ~= nil
+	return WHITELIST[gmx.GetIPAddress()] ~= nil
 end
