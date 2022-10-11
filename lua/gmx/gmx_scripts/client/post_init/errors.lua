4e1ccf1e9def3ae9692d54c9960031db5bb980e6
Earu <earu.derg@gmail.com>
2022-10-12 00:00:43 +0200

    get rid of luaerror

--- lua/gmx/gmx_scripts/client/post_init/errors.lua
+++ lua/gmx/gmx_scripts/client/post_init/errors.lua
@@ -1,34 +1,4 @@
--- clc_GMOD_ClientToServer type 2
---[[require("luaerror")
-
-local luaerror = _G.luaerror
-
-local function remove_global_stuff()
-	_G.luaerror = nil
-
-	if _G._MODULES and _G._MODULES.luaerror then
-		_G._MODULES.luaerror = nil
-		timer.Simple(0.5, remove_global_stuff)
-	end
-end
-
-remove_global_stuff()
-
-luaerror.EnableCompiletimeDetour(true)
-luaerror.EnableRuntimeDetour(true)]]
-
 local COLOR_RED = Color(255, 0, 0)
-HOOK("LuaError", function(_, full_error)
-	if not isstring(full_error) then return true end
-
-	MsgC(COLOR_RED, full_error .. "\n")
-	return true
-end)
-
-HOOK("ShutDown", function()
-	--luaerror.EnableRuntimeDetour(false)
-	--luaerror.EnableCompiletimeDetour(false)
-end)
 
 DETOUR(nil, "ErrorNoHalt", ErrorNoHalt, function(...)
 	MsgC(COLOR_RED, ...)
