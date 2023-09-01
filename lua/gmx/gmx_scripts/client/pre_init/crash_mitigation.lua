92039e8712991d7eaccb36d1ceb622d1ff14f998
Ryan <ryan.rbsr@gmail.com>
2023-09-01 22:57:35 +0200

    epic crash mitigation

--- lua/gmx/gmx_scripts/client/pre_init/crash_mitigation.lua
+++ lua/gmx/gmx_scripts/client/pre_init/crash_mitigation.lua
@@ -1,12 +1,28 @@
 local cache = {}
 
 local old_util_GetModelMeshes = _G.util.GetModelMeshes
-DETOUR(nil, "GetModelMeshes", old_util_GetModelMeshes, function(model, lod, body_grp_mask)
-	local key = string.format("%s_%d_%d", model, lod or 0, body_grp_mask or 0)
+DETOUR(_G.util, "GetModelMeshes", old_util_GetModelMeshes, function(model, lod, body_grp_mask)
+	local key = string.gsub(string.format("%s_%d_%d", model, lod or 0, body_grp_mask or 0), "[/\\]+", "_")
 	if cache[key] then return cache[key] end
+
+	local path = string.format("getmodelmeshes_cache/%s.json", key)
+	if file.Exists(path, "DATA") then
+		local contents = file.Read(path, "DATA")
+		if contents and #contents > 0 then
+			cache[key] = util.JSONToTable(contents)
+			return cache[key]
+		end
+	end
 
 	local data = old_util_GetModelMeshes(model, lod, body_grp_mask)
 	cache[key] = data
+
+	if not file.Exists("getmodelmeshes_cache", "DATA") then
+		file.CreateDir("getmodelmeshes_cache")
+	end
+
+	file.Write(path, util.TableToJSON(data))
+	print("cached new mdl meshes: " .. key)
 
 	return data
 end)
