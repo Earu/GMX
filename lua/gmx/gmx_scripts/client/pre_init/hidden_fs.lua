3d8004119f5d3aac3fa96e7719005b38bd7574b2
Ryan <ryan.rbsr@gmail.com>
2023-09-21 17:38:39 +0200

    localize some stuff

--- lua/gmx/gmx_scripts/client/pre_init/hidden_fs.lua
+++ lua/gmx/gmx_scripts/client/pre_init/hidden_fs.lua
@@ -1,3 +1,10 @@
+local string_lower = _G.string.lower
+local string_match = _G.string.match
+local string_gsub = _G.string.gsub
+local table_insert = _G.table.insert
+local pairs = _G.pairs
+local ipairs = _G.ipairs
+
 local BAD_PATHS = {
 	".*gmx.*",
 	"lua/bin/.*luaerror.*%.dll$",
@@ -16,10 +23,10 @@ local BAD_PATHS = {
 }
 
 local function should_hide(path)
-	path = string.lower(path)
+	path = string_lower(path)
 
 	for _, bad_path in pairs(BAD_PATHS) do
-		if string.match(path, bad_path) then
+		if string_match(path, bad_path) then
 			MENU_HOOK("GMXNotify", "Blocked bad path: " .. path)
 			return true
 		end
@@ -61,15 +68,15 @@ for _, detour in ipairs(read_detours) do
 			local full_path = STR_TRIM(path)
 
 			if detour.CheckUniverse then
-				local path_prefix = VFS_UNIVERSE[string.lower(universe or "")] or ""
-				if string.match(full_path, "^%/+") then
-					full_path = string.gsub(full_path, "^%/+", "")
+				local path_prefix = VFS_UNIVERSE[string_lower(universe or "")] or ""
+				if string_match(full_path, "^%/+") then
+					full_path = string_gsub(full_path, "^%/+", "")
 				end
 
 				full_path = path_prefix .. full_path
 			elseif detour.BasePath then
-				if string.match(path, "^%/+") then
-					full_path = string.gsub(path, "^%/+", "")
+				if string_match(path, "^%/+") then
+					full_path = string_gsub(path, "^%/+", "")
 				end
 
 				full_path = detour.BasePath .. full_path
@@ -78,7 +85,7 @@ for _, detour in ipairs(read_detours) do
 			if should_hide(full_path) then return detour.Default end
 
 			if full_path == "garrysmod/lua/menu/menu.lua" then
-				path = string.gsub(path, "menu.lua$", "_menu.lua")
+				path = string_gsub(path, "menu.lua$", "_menu.lua")
 			end
 
 			return old_fn(path, universe, ...)
@@ -94,15 +101,15 @@ for _, detour in ipairs(write_detours) do
 	if old_fn then
 		local new_fn = function(path, ...)
 			local full_path = STR_TRIM(path)
-			if string.match(full_path, "^%/+") then
-				full_path = string.gsub(full_path, "^%/+", "")
+			if string_match(full_path, "^%/+") then
+				full_path = string_gsub(full_path, "^%/+", "")
 			end
 
 			full_path = VFS_UNIVERSE["data"] .. full_path
 			if should_hide(full_path) then return false end
 
 			if full_path == "garrysmod/lua/menu/menu.lua" then
-				path = string.gsub(path, "menu.lua$", "_menu.lua")
+				path = string_gsub(path, "menu.lua$", "_menu.lua")
 			end
 
 			return old_fn(path, ...)
@@ -115,15 +122,15 @@ end
 local old_file_open = _G.file.Open
 local function new_file_open(file_name, mode, universe, ...)
 	local full_path = STR_TRIM(file_name)
-	if string.match(full_path, "^%/+") then
-		full_path = string.gsub(full_path, "^%/+", "")
+	if string_match(full_path, "^%/+") then
+		full_path = string_gsub(full_path, "^%/+", "")
 	end
 
-	full_path = VFS_UNIVERSE[string.lower(universe or "")] .. full_path
+	full_path = VFS_UNIVERSE[string_lower(universe or "")] .. full_path
 	if should_hide(full_path) then return end
 
 	if full_path == "garrysmod/lua/menu/menu.lua" then
-		file_name = string.gsub(file_name, "menu.lua$", "_menu.lua")
+		file_name = string_gsub(file_name, "menu.lua$", "_menu.lua")
 	end
 
 	return old_file_open(file_name, mode, universe, ...)
@@ -135,37 +142,37 @@ local old_file_find = _G.file.Find
 local function new_file_find(pattern, universe, ...)
 	local files, dirs = old_file_find(pattern, universe, ...)
 
-	local base_path = string.match(pattern, "^(.*[/\\])[^/\\]-$") or ""
-	if #base_path > 0 and not string.match(base_path, "%/$") then
+	local base_path = string_match(pattern, "^(.*[/\\])[^/\\]-$") or ""
+	if #base_path > 0 and not string_match(base_path, "%/$") then
 		base_path = base_path .. "/"
 	end
 
 	local final_files = {}
 	for _, file_name in pairs(files) do
 		local full_path = STR_TRIM(base_path .. file_name)
-		local path_prefix = VFS_UNIVERSE[string.lower(universe or "")] or ""
-		if string.match(full_path, "^%/+") then
-			full_path = string.gsub(full_path, "^%/+", "")
+		local path_prefix = VFS_UNIVERSE[string_lower(universe or "")] or ""
+		if string_match(full_path, "^%/+") then
+			full_path = string_gsub(full_path, "^%/+", "")
 		end
 
 		full_path = path_prefix .. full_path
 
 		if should_hide(full_path) then continue end
-		table.insert(final_files, file_name)
+		table_insert(final_files, file_name)
 	end
 
 	local final_dirs = {}
 	for _, dir_name in pairs(dirs) do
 		local full_path = STR_TRIM(base_path .. dir_name)
-		local path_prefix = VFS_UNIVERSE[string.lower(universe or "")] or ""
-		if string.match(full_path, "^%/+") then
-			full_path = string.gsub(full_path, "^%/+", "")
+		local path_prefix = VFS_UNIVERSE[string_lower(universe or "")] or ""
+		if string_match(full_path, "^%/+") then
+			full_path = string_gsub(full_path, "^%/+", "")
 		end
 
 		full_path = path_prefix .. full_path
 
 		if should_hide(full_path) then continue end
-		table.insert(final_dirs, dir_name)
+		table_insert(final_dirs, dir_name)
 	end
 
 	return final_files, final_dirs
