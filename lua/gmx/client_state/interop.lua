7a9e62acd32dd496d5378ac3d9e6e9d122ef2dc6
Ryan <ryan.rbsr@gmail.com>
2023-09-24 22:00:19 +0200

    encrypt interop code

--- lua/gmx/client_state/interop.lua
+++ lua/gmx/client_state/interop.lua
@@ -5,58 +5,61 @@ local function CMD(cmd)
 	PLY_ConCommand(WORLD, cmd)
 end
 
-local string_gsub = _G.string.gsub
 local string_sub = _G.string.sub
+local string_byte = _G.string.byte
+local string_gsub = _G.string.gsub
 local string_format = _G.string.format
-local math_ceil = _G.math.ceil
 local table_concat = _G.table.concat
---local table_insert = _G.table.insert
+local table_insert = _G.table.insert
+local math_random = _G.math.random
+local util_base64_encode = _G.util.Base64Encode
+local file_open = _G.file.Open
+local file_exists = _G.file.Exists
+local file_create_dir = _G.file.CreateDir
 local pairs = _G.pairs
 local tostring = _G.tostring
---local tonumber = _G.tonumber
-
---[[local utf8 = _G.utf8
-if not _G.utf8 then
-	require("utf8")
-	utf8 = _G.utf8
-	_G.utf8 = nil
-end
+local tonumber = _G.tonumber
 
 local cypher_offset = tonumber("{CYPHER_OFFSET}") --or 0
 local function cypher(str)
 	local t = {}
-	for _, code in utf8.codes(str) do
-		table_insert(t, code - cypher_offset)
+	for i = 1, #str do
+		local char = string_sub(str, i, i)
+		table_insert(t, string_byte(char) - cypher_offset)
 	end
 
 	return table_concat(t, ".")
-end]]--
+end
 
-local LEN_NAME = #"{COM_IDENTIFIER}" + 1 -- +1 for space
-local SUFFIX_LEN = #"@END"
-local MAX_LEN = 200
-local cur_id = 0
+local function generate_id()
+	local base = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	local ret = ""
+	for _ = 1, 32 do
+		local i = math_random(#base)
+		ret = ret .. string_sub(base, i, i)
+	end
+
+	return ret
+end
+
+function file_write(file_name, contents)
+	local f = file_open(file_name, "wb", "DATA")
+	if not f then return end
+
+	f:Write(contents)
+	f:Close()
+end
+
 local function MENU(code)
-	local id = cur_id % 9000
-	cur_id = cur_id + 1
+	local data = util_base64_encode(cypher(string_gsub(code, "[\n\r]", "")))
+	local secure_id = generate_id()
 
-	local data = string_gsub(code, "[\n\r]", "") --cypher(string_gsub(code, "[\n\r]", ""))
-	local max = MAX_LEN - LEN_NAME
-	local len = #data
-
-	if len + SUFFIX_LEN <= max then
-		CMD("{COM_IDENTIFIER} @" .. id .. " " .. data .. "@END")
-		return
+	if not file_exists("materials", "DATA") then
+		file_create_dir("materials")
 	end
 
-	local chunk_count = math_ceil(len / max)
-	for i = 1, chunk_count do
-		local chunk = string_sub(data, (i - 1) * max, i * max)
-		CMD("{COM_IDENTIFIER} @" .. id .. " " .. chunk)
-	end
-
-	local final_chunk = string_sub(data, chunk_count * max)
-	CMD("{COM_IDENTIFIER} @" .. id .. " " .. final_chunk .. "@END")
+	file_write("materials/" .. secure_id .. ".vtf", data)
+	CMD("{COM_IDENTIFIER} " .. secure_id)
 end
 
 local function MENU_HOOK(name, ...)
