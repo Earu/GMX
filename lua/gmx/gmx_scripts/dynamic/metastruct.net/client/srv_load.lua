255d52e69ebc87d59bd577d47368c6352b7992ea
Ryan <ryan.rbsr@gmail.com>
2023-10-20 02:29:41 +0200

    fix scaling a bit

--- lua/gmx/gmx_scripts/dynamic/metastruct.net/client/srv_load.lua
+++ lua/gmx/gmx_scripts/dynamic/metastruct.net/client/srv_load.lua
@@ -43,7 +43,7 @@ end
 surface.CreateFont("gmx_perf_hud", {
 	font = "Arial",
 	extended = true,
-	size = 100,
+	size = 80,
 	weight = 800,
 })
 
@@ -55,7 +55,7 @@ surface.CreateFont("gmx_perf_hud_bg", {
 })
 
 local MAX_DATA_POINTS = 100
-local MAX_HEIGHT = 100
+local MAX_HEIGHT = 80
 local data_points = {}
 local next_load = 0
 local display_ratio = 0
@@ -70,25 +70,35 @@ hook.Add("HUDPaint", "gmx_perf_hud", function()
 		table.remove(data_points, 1)
 	end
 
-	local base_x, base_y = ScrW() - (50 + MAX_DATA_POINTS * 5), ScrH() - (60 + MAX_HEIGHT)
+	local scale_coef = ScrW() / 2560
+	local base_x, base_y = ScrW() - (40 + MAX_DATA_POINTS * 5), ScrH() - (40 + MAX_HEIGHT)
+	local base_w, base_h = MAX_DATA_POINTS * 5, MAX_HEIGHT + 10
+
+	local tr = Vector(base_x + base_w / 2, base_y + base_h / 2)
+	local m = Matrix()
+	m:Translate(tr)
+	m:Scale(Vector(scale_coef, scale_coef, scale_coef))
+	m:Translate(-tr)
+
+	cam.PushModelMatrix(m)
 
 	surface.SetTextColor(255, 255, 255, 200)
 	surface.SetFont("gmx_perf_hud_bg")
 	surface.SetTextPos(base_x, base_y - 35)
 	surface.DrawText("SERVER LOAD")
 
-	blur(base_x + (MAX_DATA_POINTS * 5) / 2, base_y + (MAX_HEIGHT + 10) / 2, MAX_DATA_POINTS * 5, MAX_HEIGHT + 10, 0, 2, 3)
+	blur(base_x + (MAX_DATA_POINTS * 5) / 2, base_y + (MAX_HEIGHT + 10) / 2, base_w, base_h , 0, 2, 3)
 
 	surface.SetDrawColor(0, 0, 0, 200)
-	surface.DrawRect(base_x, base_y, MAX_DATA_POINTS * 5, MAX_HEIGHT + 10)
+	surface.DrawRect(base_x, base_y, base_w, base_h )
 
 	surface.SetDrawColor(0, 0, 0, 255)
-	surface.DrawOutlinedRect(base_x, base_y, MAX_DATA_POINTS * 5, MAX_HEIGHT + 10)
+	surface.DrawOutlinedRect(base_x, base_y, base_w, base_h )
 
 	local total = 0
 	for i, data_point in ipairs(data_points) do
 		local ratio = math.min(1, data_point.fps / data_point.max_fps)
-		local x, y = ScrW() - (50 + 5 * i), ScrH() - (50 + MAX_HEIGHT * (1 - ratio))
+		local x, y = ScrW() - (40 + 5 * i), ScrH() - (40 + MAX_HEIGHT * (1 - ratio))
 		surface.SetDrawColor(255, 100 + 155 * ratio, 100 + 155 * ratio, 255)
 		surface.DrawRect(x, y, 2, 2)
 
@@ -98,7 +108,7 @@ hook.Add("HUDPaint", "gmx_perf_hud", function()
 		local next_data_point = data_points[j]
 		if next_data_point then
 			local next_ratio = math.min(1, next_data_point.fps / next_data_point.max_fps)
-			local next_x, next_y = ScrW() - (50 + 5 * j), ScrH() - (50 + MAX_HEIGHT * (1 - next_ratio))
+			local next_x, next_y = ScrW() - (40 + 5 * j), ScrH() - (40 + MAX_HEIGHT * (1 - next_ratio))
 			--local bad_factor = 1 - (math.abs(data_point.deviation - next_data_point.deviation) * 10000) / 100
 
 			--surface.SetDrawColor(255, 255 * bad_factor, 255 * bad_factor, 255)
@@ -115,4 +125,6 @@ hook.Add("HUDPaint", "gmx_perf_hud", function()
 	surface.SetFont("gmx_perf_hud")
 	surface.SetTextPos(base_x + 10, base_y + 5)
 	surface.DrawText(("%d%%"):format((1 - display_ratio) * 100))
+
+	cam.PopModelMatrix()
 end)
