6a67793da734e811e88d2358f97e0a11a1752378
Earu <earu.derg@gmail.com>
2024-03-31 14:02:05 +0200

    make install script work on osx

--- install.ps1
+++ install.ps1
@@ -1,4 +1,23 @@
-# /!\ RUN AS ADMIN /!\
+# Check if the script is running on Windows
+if ($env:OS -eq "Windows_NT") {
+	$currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    if (-not ($currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+		# If running on Windows, execute with elevated privileges
+		Start-Process powershell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+		Exit
+	} else {
+		Write-Host "User is elevated."
+	}
+} else {
+	$currentUser = whoami
+	if (-not ($currentUser -eq "root")) {
+		# If running on macOS, execute with sudo
+		sudo pwsh -NoProfile -ExecutionPolicy Bypass -File "$PSCommandPath"
+		Exit
+	} else {
+		Write-Host "User is elevated."
+	}
+}
 
 $gmodPath = Read-Host -Prompt 'Input Garrys Mod path'
 $symLinks = "gmx", "menu/menu.lua", "menu/_menu.lua"
@@ -16,12 +35,13 @@ Foreach ($link in $symLinks) {
 		New-Item -ItemType SymbolicLink -Path $targetPath -Target $sourcePath
 		Write-Host "Symlink created" -ForegroundColor Green
 	} catch [System.Exception] {
-		Write-Host "Failed to create symlink $sourcePath -> $targetPath : $_.ScriptStackTrace" -ForegroundColor Red
+		Write-Host "Failed to create symlink $sourcePath -> $targetPath : $_" -ForegroundColor Red
 	}
 }
 
 Get-ChildItem "./lua/bin" -Filter *.dll | ForEach-Object {
-	$targetPath = Join-Path -Path $gmodPath -ChildPath "lua/bin/$_"
+	$targetFileName = Split-Path "$_" -Leaf  
+	$targetPath = Join-Path -Path $gmodPath -ChildPath "lua/bin/$targetFileName"
 	$sourcePath = Join-Path -Path "./" -ChildPath "lua/bin/$_"
 
 	Write-Host "Creating symlink $sourcePath -> $targetPath"
@@ -31,7 +51,8 @@ Get-ChildItem "./lua/bin" -Filter *.dll | ForEach-Object {
 			New-Item -ItemType SymbolicLink -Path $targetPath -Target $sourcePath
 			Write-Host "Symlink created" -ForegroundColor Green
 		} catch [System.Exception] {
-			Write-Host "Failed to create symlink $sourcePath -> $targetPath : $_.ScriptStackTrace" -ForegroundColor Red
+			
+			Write-Host "Failed to create symlink $sourcePath -> $targetPath : $_" -ForegroundColor Red
 		}
 	} else {
 		Write-Host "Symbolic link already exists: $targetPath"
