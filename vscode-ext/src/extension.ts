50726daf5d53ef89f3c388f2d90389861def47c9
Ryan <ryan.rbsr@gmail.com>
2024-04-02 17:20:27 +0200

    finalise vscode extension

--- vscode-ext/src/extension.ts
+++ vscode-ext/src/extension.ts
@@ -1,7 +1,9 @@
 import * as vscode from "vscode";
 import * as net from "net";
-import * as path from "path";
+import * as Path from "path";
+import * as fs from "fs";
 import { LuaVirtualFileSystemItem, LuaVirtualFileSystemProvider } from "./LuaVirtualFileSystemProvider";
+import { v4 as guid } from "uuid";
 
 type Operation = "FS_REQUEST_SYNC" | "FS_SYNC" | "FS_OPEN" | "FS_REQUEST_OPEN" | "RUN_MENU" | "RUN_CLIENT";
 type Payload<T> = {
@@ -24,7 +26,7 @@ function run(state: "menu" | "client") {
 	let document = vscode.window.activeTextEditor?.document;
 	if (!document) return;
 
-	let title = path.basename(document.uri.fsPath);
+	let title = Path.basename(document.uri.fsPath);
 	send(state === "menu" ? "RUN_MENU" : "RUN_CLIENT", { title, code: document.getText() });
 
 	vscode.window.showInformationMessage(`Running on "${state.toUpperCase()}" state`);
@@ -35,20 +37,22 @@ async function onFileSystemSync(paths: Array<string>): Promise<void> {
 	fsProvider.refresh(paths);
 }
 
+const CUR_SESSION_GUID = guid();
 async function onFileOpen(title: string, content: string) {
-	vscode.window.showInformationMessage("Opening " + title);
-	let doc = await vscode.workspace.openTextDocument({
-		content: content,
-		language: "lua",
-	});
+	let basePath = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share");
+	let sessionPath = Path.join(basePath, "gmx", CUR_SESSION_GUID);
+	let filePathChunks = Path.join(sessionPath, title).split(/[\\/]/);
+	let fileName = filePathChunks.pop() as string;
+	let fileParentPath = filePathChunks.join("/");
+	let filePath = Path.join(fileParentPath, fileName);
 
+	console.log(fileParentPath, fileName, filePath);
+
+	await new Promise(resolve => fs.mkdir(fileParentPath, { recursive: true }, resolve));
+	await new Promise(resolve => fs.writeFile(filePath, content, resolve));
+
+	let doc = await vscode.workspace.openTextDocument(vscode.Uri.file(filePath));
 	await vscode.window.showTextDocument(doc, { preserveFocus: false });
-
-	let edit = new vscode.WorkspaceEdit();
-	edit.renameFile(doc.uri, vscode.Uri.parse(title), { ignoreIfExists: true });
-	await vscode.workspace.applyEdit(edit);
-
-	await vscode.workspace.saveAll();
 }
 
 let refreshTimer: string | number | NodeJS.Timeout | undefined;
