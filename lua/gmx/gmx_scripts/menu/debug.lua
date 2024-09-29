d7c0661db6e3de0cfd308ced6dafa77ac6a518d4
Ryan <ryan.rbsr@gmail.com>
2024-09-29 21:33:25 +0200

    better printing of tables, some color tweaks

--- lua/gmx/gmx_scripts/menu/debug.lua
+++ lua/gmx/gmx_scripts/menu/debug.lua
@@ -1,3 +1,19 @@
+local function get_function_source(fn)
+	local info = debug.getinfo(fn)
+	if info.short_src == "[C]" then
+		return tostring(fn), "Native", -1, -1
+	end
+
+	local start_line, end_line = info.linedefined, info.lastlinedefined
+	local file_path = info.source:gsub("^@", "")
+	local content = file.Read(file_path, "MOD")
+	if not content or #content:Trim() == 0 then return tostring(fn), "Anonymous", -1, -1 end
+
+	local lines = ("\n"):Explode(content)
+	local fn_source = table.concat(lines, "\n", start_line, end_line)
+	return fn_source, file_path, start_line, end_line
+end
+
 local function compute_spacing_print_pos(tbl, compute_keys, max_spacing)
 	local min = 0
 	for key, value in pairs(tbl) do
@@ -18,8 +34,9 @@ end
 
 gmx.Debug = {}
 
-local RED_COLOR = Color(199, 116, 78)
-local VALUE_SPACING_MAX, INFO_SPACING_MAX = 40, 24
+local LITERAL_COLOR = Color(199, 116, 78)
+local STR_LITERAL_COLOR = Color(162, 255, 150)
+local VALUE_SPACING_MAX, INFO_SPACING_MAX = 40, 30
 function PrintTable(tbl)
 	if not istable(tbl) then
 		print(tbl)
@@ -30,38 +47,74 @@ function PrintTable(tbl)
 	MsgC(gmx.Colors.Accent, "{\n")
 
 	local min_equal_pos = compute_spacing_print_pos(tbl, true, VALUE_SPACING_MAX)
-	local min_info_pos = compute_spacing_print_pos(tbl, false, INFO_SPACING_MAX)
-
 	local tbl_keys = table.GetKeys(tbl)
 	table.sort(tbl_keys)
 
 	for _, key in ipairs(tbl_keys) do
 		local value = tbl[key]
 		local comment = type(value)
+		local value_str = tostring(value)
+		local args = { gmx.Colors.AccentAlternative, value_str }
+
 		if isfunction(value) then
 			local info = debug.getinfo(value)
 			if info.short_src == "[C]" then
 				comment = "Native"
 			else
+				local fn_source, file_path = get_function_source(value)
+				if file_path ~= "Native" and file_path ~= "Anonymous" then
+					local fn_params = fn_source:Split("\n")[1]:match("function.*%((.*)%)")
+
+					args = { gmx.Colors.Accent, "function", gmx.Colors.Text, " (", fn_params, ")" }
+					value_str = "function (" .. fn_params .. ")"
+				else
+					args = { gmx.Colors.Accent, value_str }
+				end
+
 				comment = info.short_src .. ":" .. info.linedefined
+			end
+		elseif isstring(value) then
+			value_str = "\"" .. value_str .. "\""
+			args = { STR_LITERAL_COLOR, value_str }
+		elseif isnumber(value) or isbool(value) then
+			args = { LITERAL_COLOR, value_str }
+		elseif IsColor(value) or type(value) == "Vector" then
+			local type_name = IsColor(value) and "Color" or "Vector"
+			args = { gmx.Colors.AccentAlternative, type_name, gmx.Colors.Text, " (" }
+			value_str = type_name .. " ("
+
+			local col_components = { value:Unpack() }
+			for i, col_component in ipairs(col_components) do
+				table.insert(args, LITERAL_COLOR)
+				table.insert(args, tostring(col_component))
+				table.insert(args, gmx.Colors.Text)
+
+				value_str = value_str .. tostring(col_component)
+
+				if i < #col_components then
+					table.insert(args, ", ")
+					value_str = value_str .. ", "
+				end
+			end
+
+			table.insert(args, gmx.Colors.Text)
+			table.insert(args, ")")
+			value_str = value_str .. ")"
+
+			if type_name == "Color" then
+				comment = { "Color (", value, "███", gmx.Colors.TextAlternative, ")" }
+			end
+		elseif istable(value) then
+			if table.Count(value) == 0 then
+				value_str = "{}"
+				args = { gmx.Colors.Text, value_str }
+				comment = "table (empty)"
 			end
 		end
 
-		local value_color = gmx.Colors.AccentAlternative
-		if isstring(value) then
-			value_color = gmx.Colors.Accent
-		elseif isnumber(value) or isbool(value) then
-			value_color = RED_COLOR
-		end
-
-		local value_str = tostring(value)
-		if isstring(value) then
-			value_str = "\"" .. value_str .. "\""
-		end
-
 		local spacing_info = ""
-		if #value_str < min_info_pos then
-			spacing_info = (" "):rep(min_info_pos - #value_str)
+		if #value_str < INFO_SPACING_MAX then
+			spacing_info = (" "):rep(INFO_SPACING_MAX - #value_str)
 		end
 
 		local key_name = tostring(key)
@@ -71,7 +124,22 @@ function PrintTable(tbl)
 			spacing_value = (" "):rep(min_equal_pos - key_len)
 		end
 
-		MsgC(gmx.Colors.Accent, "\t[", gmx.Colors.Text, key_name, gmx.Colors.Accent, "]", gmx.Colors.Text, spacing_value .. " = ", value_color, value_str, gmx.Colors.TextAlternative, spacing_info .. " -- " .. comment .. "\n")
+		local final_args = {
+			gmx.Colors.Accent, "\t[", gmx.Colors.Text, key_name, gmx.Colors.Accent, "]",
+			gmx.Colors.Text, spacing_value .. " = "
+		}
+
+		final_args = table.Add(final_args, args)
+		final_args = table.Add(final_args, { gmx.Colors.TextAlternative, spacing_info .. " -- "})
+
+		if isstring(comment) then
+			table.insert(final_args, comment .. "\n")
+		elseif istable(comment) then
+			final_args = table.Add(final_args, comment)
+			table.insert(final_args, "\n")
+		end
+
+		MsgC(unpack(final_args))
 	end
 
 	MsgC(gmx.Colors.Accent, "}\n")
@@ -83,26 +151,10 @@ FindMetaTable("Vector").__tostring = function(self)
 	return ("Vector (%d, %d, %d)"):format(self.x, self.y, self.z)
 end
 
-local function get_function_source(fn)
-	local info = debug.getinfo(fn)
-	if info.short_src == "[C]" then
-		return tostring(fn), "Native", -1, -1
-	end
-
-	local start_line, end_line = info.linedefined, info.lastlinedefined
-	local file_path = info.source:gsub("^@", "")
-	local content = file.Read(file_path, "MOD")
-	if not content or #content:Trim() == 0 then return tostring(fn), "Anonymous", -1, -1 end
-
-	local lines = ("\n"):Explode(content)
-	local fn_source = table.concat(lines, "\n", start_line, end_line)
-	return fn_source, file_path, start_line, end_line
-end
-
 local function markup_keyword(match)
 	local start_pos, end_pos = match:find("[a-zA-Z]+")
 	local keyword = match:sub(start_pos, end_pos)
-	local color = (keyword == "true" or keyword == "false") and RED_COLOR or gmx.Colors.AccentAlternative
+	local color = (keyword == "true" or keyword == "false") and LITERAL_COLOR or gmx.Colors.Accent
 	return ("%s<color=%d,%d,%d>%s</color>%s"):format(
 		match:sub(1, start_pos - 1),
 		color.r, color.g, color.b,
@@ -159,9 +211,9 @@ function PrintCode(code)
 
 	-- strings literals
 	code = code
-		:gsub("'.-'", markup_with_color(gmx.Colors.Accent))
-		:gsub("\".-\"", markup_with_color(gmx.Colors.Accent))
-		:gsub("[^%-]%[%[.-%]%]", markup_with_color(gmx.Colors.Accent))
+		:gsub("'.-'", markup_with_color(STR_LITERAL_COLOR))
+		:gsub("\".-\"", markup_with_color(STR_LITERAL_COLOR))
+		:gsub("[^%-]%[%[.-%]%]", markup_with_color(STR_LITERAL_COLOR))
 
 	-- comments
 	code = code
